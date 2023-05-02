3e0a8bdfc35c7efd7e5ef34052587b5369947715
raphaelmwachiti <raphaelmwachiti@gmail.com>
2023-05-02 16:41:51 -0700

    Folder added

---
+++ app/index.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'; 
+
+const Home = () => { 
+    return (
+        <View> 
+            <Text>Hom a paragraph thaat should show on the application nothign from</Text>
+        </View>
+    )
+} 
+
+export default Home; 
