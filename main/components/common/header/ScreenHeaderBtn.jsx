730635aa3bacf6e27c248eb0d4d6549304f95cc9
raphaelmwachiti <raphaelmwachiti@gmail.com>
2023-05-09 14:45:03 -0700

    edits to files

--- main/components/common/header/ScreenHeaderBtn.jsx
+++ main/components/common/header/ScreenHeaderBtn.jsx
@@ -2,9 +2,9 @@ import { Image, TouchableOpacity } from "react-native";
 
 import styles from "./screenheader.style";
 
-const ScreenHeaderBtn = ({ iconUrl, dimension, handlePress }) => {
+const ScreenHeaderBtn = ({ iconUrl, dimension, handlePress, marginL, marginR}) => {
   return (
-    <TouchableOpacity style={styles.btnContainer} onPress={handlePress}>
+    <TouchableOpacity style={styles.btnContainer(marginL, marginR)} onPress={handlePress}>
       <Image
         source={iconUrl}
         resizeMode='cover'
--- main/components/common/header/screenheader.style.js
+++ main/components/common/header/screenheader.style.js
@@ -3,14 +3,17 @@ import { StyleSheet } from "react-native";
 import { COLORS, SIZES } from "../../../constants";
 
 const styles = StyleSheet.create({
-  btnContainer: {
+  btnContainer: (marginL, marginR) => ({
     width: 40,
     height: 40,
     backgroundColor: COLORS.white,
     borderRadius: SIZES.small / 1.25,
     justifyContent: "center",
     alignItems: "center",
-  },
+    marginLeft: marginL,
+    marginRight: marginR,
+
+  }),
   btnImg: (dimension) => ({
     width: dimension,
     height: dimension,
