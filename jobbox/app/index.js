ff71a56f1e55b3b1cb2d80bfc975940e14e5366b
beratcelik1 <beratcelik8128@gmail.com>
2023-05-10 23:45:45 -0700

    navigation bar and pages are arranged with logo

--- jobbox/app/index.js
+++ jobbox/app/index.js
@@ -1,35 +1,66 @@
 import React from 'react';
+import { Image } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-import Activity from './app/screens/Activity';
-import Profile from './app/screens/Profile';
-import Services from './app/screens/Services';
-import Hire from './app/screens/Home/Hire';
-import Work from './app/screens/Home/Work';
+import Activity from './screens/Activity';
+import Profile from './screens/Profile';
+import Services from './screens/Services';
+import Hire from './screens/Home/Hire';
+import Work from './screens/Home/Work';
 
-const Stack = createStackNavigator();
-const Tab = createBottomTabNavigator();
+const logo = require('./assets/images/jobboxlogo.png');
 
-function HomeTabs() {
+const TopTab = createMaterialTopTabNavigator();
+const BottomTab = createBottomTabNavigator();
+
+function HomeTopTabs() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Hire" component={Hire} />
-      <Tab.Screen name="Work" component={Work} />
-    </Tab.Navigator>
+    <TopTab.Navigator initialRouteName="Hire">
+      <TopTab.Screen name="Hire" component={Hire} />
+      <TopTab.Screen name="Work" component={Work} />
+    </TopTab.Navigator>
   );
 }
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeTabs} options={{title: 'Home'}} />
-        <Stack.Screen name="Activity" component={Activity} options={{title: 'Activity'}} />
-        <Stack.Screen name="Profile" component={Profile} options={{title: 'Profile'}} />
-        <Stack.Screen name="Services" component={Services} options={{title: 'Services'}} />
-      </Stack.Navigator>
+    <NavigationContainer independent={true}>
+      <BottomTab.Navigator initialRouteName="Home">
+        <BottomTab.Screen 
+          name="Home" 
+          component={HomeTopTabs} 
+          options={{ 
+            title: 'Home',
+            headerTitle: () => <Image source={logo} style={{ width: 30, height: 30 }} />
+          }}
+        />
+        <BottomTab.Screen 
+          name="Services" 
+          component={Services} 
+          options={{ 
+            title: 'Services',
+            headerTitle: () => <Image source={logo} style={{ width: 30, height: 30 }} />
+          }}
+        />
+        <BottomTab.Screen 
+          name="Activity" 
+          component={Activity} 
+          options={{ 
+            title: 'Activity',
+            headerTitle: () => <Image source={logo} style={{ width: 30, height: 30 }} />
+          }}
+        />
+        <BottomTab.Screen 
+          name="Profile" 
+          component={Profile} 
+          options={{ 
+            title: 'Profile',
+            headerTitle: () => <Image source={logo} style={{ width: 30, height: 30 }} />
+          }}
+        />
+      </BottomTab.Navigator>
     </NavigationContainer>
   );
 }
---
+++ jobbox/app/screens/Home/Work.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function Work() {
+    return (
+        <View>
+            <Text>Work Screen</Text>
+        </View>
+    );
+}
