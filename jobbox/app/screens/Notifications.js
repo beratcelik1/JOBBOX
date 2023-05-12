d64fb59ed37a1da2e3b4dccd7edf7a2ba038dfb8
beratcelik1 <beratcelik8128@gmail.com>
2023-05-11 18:07:42 -0700

    messages/notifications back navigation bar logo

--- jobbox/app/screens/Notifications.js
+++ jobbox/app/screens/Notifications.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button  } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 export default function Notifications() {
+    const navigation = useNavigation();
+  
     return (
-        <View>
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+         <View>
             <Text>Notifications Screen</Text>
         </View>
+        <Button title="Go Back" onPress={() => navigation.goBack()} />
+      </View>
     );
 }
