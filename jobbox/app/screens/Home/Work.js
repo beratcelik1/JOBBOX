84aeaf683bcb424e5e425a13cfae4e761f3ee30b
raphaelmwachiti <raphaelmwachiti@gmail.com>
2023-05-11 19:04:56 -0700

    Search bar and filter added

--- jobbox/app/screens/Home/Work.js
+++ jobbox/app/screens/Home/Work.js
@@ -1,10 +1,106 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+import { Ionicons } from '@expo/vector-icons'; // you might need to install this package
 
 export default function Work() {
+    const [searchQuery, setSearchQuery] = useState('');
+
+    const handleSearch = () => {
+        // handle search logic here
+        console.log(searchQuery);
+    };
+
     return (
-        <View>
-            <Text>Work Screen</Text>
+        <View style={styles.container}>
+            <View style={styles.searchSection}>
+                <Ionicons style={styles.searchIcon} name="ios-search" size={20} color="#000" />
+                <TextInput
+                    style={styles.searchInput}
+                    onChangeText={setSearchQuery}
+                    value={searchQuery}
+                    placeholder="What Job are you looking for..."
+                    placeholderTextColor="gray"
+                />
+                <TouchableOpacity
+                    onPress={handleSearch}
+                    style={styles.searchButton}
+                >
+                    <Text style={styles.buttonText}>Search</Text>
+                </TouchableOpacity>
+            </View>
+            <View style={styles.filterSection}>
+                <Text>Filter by:</Text>
+                <TouchableOpacity>
+                    <Text style={styles.filterOption}>Category</Text>
+                </TouchableOpacity>
+                <TouchableOpacity>
+                    <Text style={styles.filterOption}>Rating</Text>
+                </TouchableOpacity>
+                <TouchableOpacity>
+                    <Text style={styles.filterOption}>Time</Text>
+                </TouchableOpacity>
+                <TouchableOpacity>
+                    <Text style={styles.filterOption}>Pay</Text>
+                </TouchableOpacity>
+            </View>
         </View>
     );
 }
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 10,
+        backgroundColor: '#f5f5f5',
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginBottom: 20,
+    },
+    searchSection: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#fff',
+        borderRadius: 50,
+        paddingLeft: 10,
+    },
+    searchIcon: {
+        padding: 10,
+    },
+    searchInput: {
+        flex: 1,
+        paddingTop: 10,
+        paddingRight: 10,
+        paddingBottom: 10,
+        paddingLeft: 0,
+        backgroundColor: '#fff',
+        color: '#424242',
+    },
+    searchButton: {
+        backgroundColor: '#4683fc',
+        borderRadius: 50,
+        width: 100,
+        height: 40,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginRight: 10,
+    },
+    buttonText: {
+        color: 'white',
+        fontWeight: 'bold',
+    },
+    filterSection: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        marginTop: 20,
+    },
+    filterOption: {
+        color: '#4683fc',
+        fontWeight: 'bold',
+        fontSize: 16,
+    },
+});
