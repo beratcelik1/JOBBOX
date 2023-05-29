1922380e5cc209ade69f48d04613f7a337a6671f
beratcelik1 <beratcelik8128@gmail.com>
2023-05-28 18:20:38 -0700

    Category integration is done

--- jobbox/app/screens/Services/Services.js
+++ jobbox/app/screens/Services/Services.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, FlatList, Text, TouchableOpacity, StyleSheet } from 'react-native';
 import CategoryItem from './CategoryItem'; // make sure the import path is correct
 import Icon from 'react-native-vector-icons/MaterialIcons';
@@ -51,22 +51,47 @@ const categories = [
 ];
 
 export default function Services({ navigation }) {
+  const predefinedCategories = [
+    { id: '1', title: 'Web Development', iconName: 'web' },
+    { id: '2', title: 'Graphic Design', iconName: 'palette' },
+    { id: '3', title: 'Content Writing', iconName: 'create' },
+    { id: '4', title: 'Marketing', iconName: 'campaign' },
+    { id: '5', title: 'Mobile App Development', iconName: 'mobile-friendly' },
+    { id: '6', title: 'Home Cleaning', iconName: 'cleaning-services' },
+    { id: '7', title: 'Gardening', iconName: 'grass' },
+    { id: '8', title: 'Dog Walking', iconName: 'pets' },
+    { id: '9', title: 'Grocery Delivery', iconName: 'local-grocery-store' },
+    { id: '10', title: 'Moving', iconName: 'local-shipping' },
+    // add more categories as needed
+  ];
+  const [categoriesWithJobs, setCategoriesWithJobs] = useState([]); 
+
+  useEffect(() => {
+    fetch(`https://tranquil-ocean-74659.herokuapp.com/jobs`)
+    .then(response => response.json())
+    .then(data => {
+      const uniqueCategories = [...new Set(data.map(job => job.category))];
+      setCategoriesWithJobs(uniqueCategories);
+    })
+    .catch(error => console.error('Error:', error));
+  }, []);
+  
   return (
     <View style={styles.container}>
-      <FlatList 
-        data={categories}
+    <FlatList 
+        data={predefinedCategories}
         keyExtractor={item => item.id}
         renderItem={({ item }) => (
-          <TouchableOpacity
+        <TouchableOpacity
             style={styles.categoryCard}
             onPress={() => navigation.navigate('Category', { category: item.title })}
-          >
+        >
             <Icon name={item.iconName} size={24} color="#4683fc" />
             <Text style={styles.categoryText}>{item.title}</Text>
-          </TouchableOpacity>
+        </TouchableOpacity>
         )}
         numColumns={2}
-      />
+    />
     </View>
   );
 }
