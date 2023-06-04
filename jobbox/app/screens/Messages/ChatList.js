59c18a3be5360be5506a1804e22412149c067782
ammaarkhan <ammaarkhan03@gmail.com>
2023-06-03 21:28:40 -0700

    add search in messages

--- jobbox/app/screens/Messages/ChatList.js
+++ jobbox/app/screens/Messages/ChatList.js
@@ -2,11 +2,41 @@ import React, { useEffect, useState } from 'react';
 import { StyleSheet, FlatList, Text, TouchableOpacity, View } from 'react-native';
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { SearchBar } from 'react-native-elements';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 10,
+    marginHorizontal: 10,
+  },
+  card: {
+    backgroundColor: '#fff',
+    padding: 15,
+    marginBottom: 10,
+    borderRadius: 5,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  label: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+});
 
 function ChatList({ navigateToChat }) {
   const [users, setUsers] = useState([]);
-  // const currentUserID = "YOUR_CURRENT_USER_ID"; // replace with actual value
+  const [search, setSearch] = useState('');
+  const [filteredDataSource, setFilteredDataSource] = useState([]);
+  const [searchResults, setSearchResults] = useState([]);
   const [user, setUser] = useState({});
+  const [conversations, setConversations] = useState([]);
 
   // Fetch the current user's information
   useEffect(() => {
@@ -16,14 +46,43 @@ function ChatList({ navigateToChat }) {
         const response = await axios.get('https://tranquil-ocean-74659.herokuapp.com/auth/user/me', {
           headers: { Authorization: `Bearer ${token}` },
         });
-        // console.log("Response data: ", response.data); // log the response data
         setUser(response.data);
+        
+        // Get conversations for current user after fetching user data
+        try {
+          const res = await axios.get("https://tranquil-ocean-74659.herokuapp.com/conversations/" + response.data._id);
+          setConversations(res.data);
+        } catch (err) {
+          console.log(err);
+        }
       } catch (err) {
         console.error("Failed to fetch user data: ", err);
-      } }
-      fetchUserData();
-  });
+      }
+    }
+    fetchUserData();
+  }, []);
 
+  // Get conversations for current user
+  useEffect(() => {
+    const getConversations = async () => {
+      try {
+        const res = await axios.get("https://tranquil-ocean-74659.herokuapp.com/conversations/" + user._id);
+        setConversations(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    if (user._id) {
+      getConversations();
+    }
+  }, [user._id]);
+
+  // Extract all conversation partner ids
+  const conversationPartnerIds = conversations.map((conversation) => 
+    conversation.members.find((memberId) => memberId !== user._id)
+  );
+
+  // get list of all users
   useEffect(() => {
     fetch('http://tranquil-ocean-74659.herokuapp.com/auth/users')
       .then(response => {
@@ -32,9 +91,27 @@ function ChatList({ navigateToChat }) {
         }
         return response.json();
       })
-      .then(users => setUsers(users))
+      .then(users => {
+        setUsers(users);
+        setFilteredDataSource(users);
+      })
       .catch(error => console.error('Error:', error));
   }, []);
+
+  const searchFilterFunction = (text) => {
+    if (text) {
+      const newData = users.filter(item => {
+        const itemData = item.firstname.toUpperCase();
+        const textData = text.toUpperCase();
+        return itemData.indexOf(textData) > -1;
+      });
+      setSearchResults(newData);
+      setSearch(text);
+    } else {
+      setSearchResults([]);
+      setSearch(text);
+    }
+  };
 
   const handleChatNavigation = async (receiverId) => {
     let conversation = await axios.get(`http://tranquil-ocean-74659.herokuapp.com/conversations/find/${user._id}/${receiverId}`);
@@ -50,26 +127,38 @@ function ChatList({ navigateToChat }) {
   };
 
   return (
-    <View>
-      <FlatList
-        data={users}
-        keyExtractor={(item, index) => item._id.toString()}
-        renderItem={({ item }) => (
-          <TouchableOpacity onPress={() => handleChatNavigation(item._id)}>
-            <Text style={styles.item}>{item.firstname}</Text>
-          </TouchableOpacity>
-        )}
+    <View style={styles.container}>
+      <SearchBar
+        round
+        searchIcon={{ size: 24 }}
+        onChangeText={(text) => searchFilterFunction(text)}
+        onClear={(text) => searchFilterFunction('')}
+        placeholder="Search..."
+        value={search}
       />
+      {search.length > 0 ?
+        <FlatList
+          data={searchResults}
+          keyExtractor={(item, index) => item._id.toString()}
+          renderItem={({ item }) => (
+            <TouchableOpacity style={styles.card} onPress={() => handleChatNavigation(item._id)}>
+              <Text style={styles.label}>{item.firstname}</Text>
+            </TouchableOpacity>
+          )}
+        />
+        :
+        <FlatList
+          data={filteredDataSource.filter(user => conversationPartnerIds.includes(user._id))}
+          keyExtractor={(item, index) => item._id.toString()}
+          renderItem={({ item }) => (
+            <TouchableOpacity style={styles.card} onPress={() => handleChatNavigation(item._id)}>
+              <Text style={styles.label}>{item.firstname}</Text>
+            </TouchableOpacity>
+          )}
+        />
+      }
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  item: {
-    padding: 10,
-    fontSize: 18,
-    height: 44,
-  },
-});
 
 export default ChatList;
--- jobbox/app/screens/Messages/ChatRoom.js
+++ jobbox/app/screens/Messages/ChatRoom.js
@@ -1,70 +1,71 @@
-// chatroom.js
 import React, { useState, useEffect } from 'react';
 import { GiftedChat } from 'react-native-gifted-chat';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import axios from 'axios';
+import { Text } from 'react-native';
 
 const ChatRoom = ({ currentChatId }) => {
   const [conversations, setConversations] = useState([]);
-  // const [currentChat, setCurrentChat] = useState(null);
   const [messages, setMessages] = useState([]);
-  const [newMessage, setNewMessage] = useState("");
   const [user, setUser] = useState({});
+  const [loading, setLoading] = useState(true);
 
   const currentChat = currentChatId;
-  // setCurrentChat(currentChatId);
+
   // Fetch the current user's information
   useEffect(() => {
     const fetchUserData = async () => {
+      setLoading(true);
       try {
         const token = await AsyncStorage.getItem('token');
         const response = await axios.get('https://tranquil-ocean-74659.herokuapp.com/auth/user/me', {
           headers: { Authorization: `Bearer ${token}` },
         });
-        // console.log("Response data: ", response.data); // log the response data
         setUser(response.data);
       } catch (err) {
         console.error("Failed to fetch user data: ", err);
-      } }
-      fetchUserData();
-  });
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchUserData();
+  }, []);
 
-  // get conversations for current user
+  // Get conversations for current user
   useEffect(() => {
     const getConversations = async () => {
       try {
         const res = await axios.get("https://tranquil-ocean-74659.herokuapp.com/conversations/" + user._id);
-        // console.log(res);
         setConversations(res.data);
       } catch (err) {
         console.log(err);
       }
     };
-    getConversations();
+    if (user._id) {
+      getConversations();
+    }
   }, [user._id]);
 
   useEffect(() => {
     const getMessages = async () => {
-        if (currentChat) {
-            try {
-                const res = await axios.get("https://tranquil-ocean-74659.herokuapp.com/messages/" + currentChat._id);
-                const formattedMessages = res.data.map((message) => ({
-                    _id: message._id,
-                    text: message.text,
-                    createdAt: new Date(message.createdAt),
-                    user: {
-                        _id: message.sender,
-                        // name: message.sender,
-                        // avatar: 'https://placeimg.com/140/140/any',
-                    },
-                })).reverse();;
-                setMessages(formattedMessages);
-            } catch (err) {
-                console.log(err);
-            }
-        } else {
-            console.log('no chat info')
+      if (currentChat) {
+        try {
+          const res = await axios.get("https://tranquil-ocean-74659.herokuapp.com/messages/" + currentChat._id);
+          const formattedMessages = res.data.map((message) => ({
+            _id: message._id,
+            text: message.text,
+            createdAt: new Date(message.createdAt),
+            user: {
+              _id: message.sender,
+            },
+          })).reverse();
+          setMessages(formattedMessages);
+        } catch (err) {
+          console.log(err);
         }
+      } else {
+        console.log('no chat info')
+      }
     };
     getMessages();
   }, [currentChat]);
@@ -72,18 +73,12 @@ const ChatRoom = ({ currentChatId }) => {
   const handleSend = async (messagesArray) => {
     // Extract text from the first message in the array
     const newMessage = messagesArray[0].text;
-    
     const message = {
       sender: user._id,
       text: newMessage,
       conversationId: currentChat._id,
     };
 
-    // const receiverId = currentChat.members.find(
-    //   (member) => member !== user._id
-    // );
-    console.log("user id:",user._id);
-    console.log("chat id:",currentChat.members);
     try {
       const res = await axios.post("https://tranquil-ocean-74659.herokuapp.com/messages/", message);
       const formattedMessage = {
@@ -94,27 +89,22 @@ const ChatRoom = ({ currentChatId }) => {
           _id: res.data.sender,
         },
       };
-      setMessages([formattedMessage, ...messages]);
+      setMessages(previousMessages => GiftedChat.append(previousMessages, formattedMessage));
     } catch (err) {
       console.log(err);
     }
   };
-  //   try {
-  //     const res = await axios.post("https://tranquil-ocean-74659.herokuapp.com/messages/", message);
-  //     setMessages([...messages, res.data]);
-  //     // setNewMessage("");
-  //   } catch (err) {
-  //     console.log(err);
-  //   }
-  // };
+
+  if (loading) {
+    return <Text>Loading...</Text>; // replace with your actual loading component or spinner
+  }
 
   return (
     <GiftedChat
       messages={messages}
-      // onSend={e => handleSend(e)}
       onSend={messages => handleSend(messages)}
-      // user={{ _id: user._id, name: `${user.firstname} ${user.lastname}` }}
       user={{ _id: user._id }}
+      renderAvatar={null}
     />
   );
 };
@@ -122,17 +112,141 @@ const ChatRoom = ({ currentChatId }) => {
 export default ChatRoom;
 
 
-// try {
-    //   const res = await axios.post("https://tranquil-ocean-74659.herokuapp.com/messages/", message);
-    //   const formattedMessage = {
-    //     _id: res.data._id,
-    //     text: res.data.text,
-    //     createdAt: new Date(res.data.createdAt),
-    //     user: {
-    //       _id: res.data.sender,
-    //     },
-    //   };
-    //   setMessages((previousMessages) => GiftedChat.append(previousMessages, formattedMessage));
-    // } catch (err) {
-    //   console.log(err);
-    // }
+// // chatroom.js
+// import React, { useState, useEffect } from 'react';
+// import { GiftedChat } from 'react-native-gifted-chat';
+// import AsyncStorage from '@react-native-async-storage/async-storage';
+// import axios from 'axios';
+
+// const ChatRoom = ({ currentChatId }) => {
+//   const [conversations, setConversations] = useState([]);
+//   // const [currentChat, setCurrentChat] = useState(null);
+//   const [messages, setMessages] = useState([]);
+//   const [newMessage, setNewMessage] = useState("");
+//   const [user, setUser] = useState({});
+
+//   const currentChat = currentChatId;
+//   // setCurrentChat(currentChatId);
+//   // Fetch the current user's information
+//   useEffect(() => {
+//     const fetchUserData = async () => {
+//       try {
+//         const token = await AsyncStorage.getItem('token');
+//         const response = await axios.get('https://tranquil-ocean-74659.herokuapp.com/auth/user/me', {
+//           headers: { Authorization: `Bearer ${token}` },
+//         });
+//         // console.log("Response data: ", response.data); // log the response data
+//         setUser(response.data);
+//       } catch (err) {
+//         console.error("Failed to fetch user data: ", err);
+//       } }
+//       fetchUserData();
+//   });
+
+//   // get conversations for current user
+//   useEffect(() => {
+//     const getConversations = async () => {
+//       try {
+//         const res = await axios.get("https://tranquil-ocean-74659.herokuapp.com/conversations/" + user._id);
+//         // console.log(res);
+//         setConversations(res.data);
+//       } catch (err) {
+//         console.log(err);
+//       }
+//     };
+//     getConversations();
+//   }, [user._id]);
+
+//   useEffect(() => {
+//     const getMessages = async () => {
+//         if (currentChat) {
+//             try {
+//                 const res = await axios.get("https://tranquil-ocean-74659.herokuapp.com/messages/" + currentChat._id);
+//                 const formattedMessages = res.data.map((message) => ({
+//                     _id: message._id,
+//                     text: message.text,
+//                     createdAt: new Date(message.createdAt),
+//                     user: {
+//                         _id: message.sender,
+//                         // name: message.sender,
+//                         // avatar: 'https://placeimg.com/140/140/any',
+//                     },
+//                 })).reverse();;
+//                 setMessages(formattedMessages);
+//             } catch (err) {
+//                 console.log(err);
+//             }
+//         } else {
+//             console.log('no chat info')
+//         }
+//     };
+//     getMessages();
+//   }, [currentChat]);
+
+//   const handleSend = async (messagesArray) => {
+//     // Extract text from the first message in the array
+//     const newMessage = messagesArray[0].text;
+    
+//     const message = {
+//       sender: user._id,
+//       text: newMessage,
+//       conversationId: currentChat._id,
+//     };
+
+//     // const receiverId = currentChat.members.find(
+//     //   (member) => member !== user._id
+//     // );
+//     console.log("user id:",user._id);
+//     console.log("chat id:",currentChat.members);
+//     try {
+//       const res = await axios.post("https://tranquil-ocean-74659.herokuapp.com/messages/", message);
+//       const formattedMessage = {
+//         _id: res.data._id,
+//         text: res.data.text,
+//         createdAt: new Date(res.data.createdAt),
+//         user: {
+//           _id: res.data.sender,
+//         },
+//       };
+//       setMessages([formattedMessage, ...messages]);
+//     } catch (err) {
+//       console.log(err);
+//     }
+//   };
+//   //   try {
+//   //     const res = await axios.post("https://tranquil-ocean-74659.herokuapp.com/messages/", message);
+//   //     setMessages([...messages, res.data]);
+//   //     // setNewMessage("");
+//   //   } catch (err) {
+//   //     console.log(err);
+//   //   }
+//   // };
+
+//   return (
+//     <GiftedChat
+//       messages={messages}
+//       // onSend={e => handleSend(e)}
+//       onSend={messages => handleSend(messages)}
+//       // user={{ _id: user._id, name: `${user.firstname} ${user.lastname}` }}
+//       user={{ _id: user._id }}
+//     />
+//   );
+// };
+
+// export default ChatRoom;
+
+
+// // try {
+//     //   const res = await axios.post("https://tranquil-ocean-74659.herokuapp.com/messages/", message);
+//     //   const formattedMessage = {
+//     //     _id: res.data._id,
+//     //     text: res.data.text,
+//     //     createdAt: new Date(res.data.createdAt),
+//     //     user: {
+//     //       _id: res.data.sender,
+//     //     },
+//     //   };
+//     //   setMessages((previousMessages) => GiftedChat.append(previousMessages, formattedMessage));
+//     // } catch (err) {
+//     //   console.log(err);
+//     // }
