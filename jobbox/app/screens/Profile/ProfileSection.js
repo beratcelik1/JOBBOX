012b659f711697402db524b8f0b22e34bcb9ace6
Jeena01 <jeenajavahar@gmail.com>
2023-06-16 17:52:19 -0700

    Update ProfileSection.js

--- jobbox/app/screens/Profile/ProfileSection.js
+++ jobbox/app/screens/Profile/ProfileSection.js
@@ -140,7 +140,27 @@ const ProfileSection = ({ route, navigation }) => {
       ? section.data 
       : [{ name: '', relationship: '', recommendation: '' }]
   );
-  
+  const [isKeyboardVisible, setKeyboardVisible] = useState(false);
+
+ useEffect(() => {
+    const keyboardDidShowListener = Keyboard.addListener(
+      'keyboardDidShow',
+      () => {
+        setKeyboardVisible(true); // or some other action
+      }
+    );
+    const keyboardDidHideListener = Keyboard.addListener(
+      'keyboardDidHide',
+      () => {
+        setKeyboardVisible(false); // or some other action
+      }
+    );
+
+    return () => {
+      keyboardDidHideListener.remove();
+      keyboardDidShowListener.remove();
+    };
+  }, []);
   
   useEffect(() => {
     if (section.title === 'About') {
