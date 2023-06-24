cf170840b3e95ef7bcfbce6827e4bb07cbf4ab79
raphaelmwachiti <raphaelmwachiti@gmail.com>
2023-06-24 14:19:44 -0700

    commiting

--- jobbox/app/screens/Home/Hire.js
+++ jobbox/app/screens/Home/Hire.js
@@ -266,10 +266,17 @@ function ArchivedJobsScreen({ navigation }) {
         </View>
       </View>
 
-      <View style={{flexDirection: 'column', justifyContent: 'flex-start'}}>
-        <Text style={styles.jobDescription}>Start Date/Time {startDate}/{startTime}</Text>
-        <Text style={styles.jobDescription}>End Date/Time {endDate}/{endTime}</Text>
-      </View>
+      <View style={{  justifyContent: 'flex-start'}}> 
+          <View style={{ flexDirection: 'row', justifyContent: 'flex-start'}}> 
+            <Text style = {{marginTop: 4, color: '#fff', fontWeight: '700'}}>Start Date:  </Text>
+            <Text style={styles.jobDescription}>{startDate}</Text>
+          </View> 
+
+          <View style={{ flexDirection: 'row', justifyContent: 'flex-start'}}> 
+            <Text style = {{marginTop: 4, color: '#fff', fontWeight: '700'}}>Start Time:  </Text>
+            <Text style={styles.jobDescription}>{startTime}</Text>
+          </View>
+        </View>
 
       <TouchableOpacity 
         style={styles.button}
